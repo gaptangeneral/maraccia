4b45571efd54a52b16189c125b172b215a0aaaf7
marakesh46 <Marakesh1907@gmail.com>
2024-03-10 07:19:24 +0300

    secre

--- inventorySystem/settings.py
+++ inventorySystem/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-DEBUG = os.environ.get("SECRET_KEY")
+SECRET_KEY='92737fec5595924071871e76b67581e3'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG","False")=="true"
